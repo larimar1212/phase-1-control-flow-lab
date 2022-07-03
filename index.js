0a6cfc71d479a360a9e82fcbb6580c45b3aceafb
Larimar Fondeur <larimarfondeur@gmail.com>
2022-07-03 14:44:23 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!"
+  }
+
+
+} else if (feet <= 2000) {
+  return "That will be twenty bucks."
+} else if (2000 < feet && feet <= 2500) {  // && for describing in between
+  return "I will gladly take your thirty bucks" 
+}else {
+  return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+// use a ternary operator to return the correct response based
+// on the desired destination of the passenger 
+
+
+function ternaryCheckCity(city ){
+  return city === "NYC" ? "Ok, sounds good." : "No go";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      case  "not as generous":
+        return "Thank you ";
+        default:
+          return "Bye.";
+
+  }
 }
